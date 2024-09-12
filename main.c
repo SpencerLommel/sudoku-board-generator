78741fbb0f775ba47d44e482bc5d346903790e62
Spencer <spencerlommel@gmail.com>
2024-09-11 22:58:27 -0500

    sudoku.c layout

---
+++ main.c
@@ -0,0 +1,14 @@
+#include "sudoku.h"
+
+// Test program for sudoku generator
+// Spencer Lommel
+// Sept. 11th, 2024
+
+int main() {
+    sudoku_board_t sudoku_board;
+
+    sudoku_init(&sudoku_board);
+
+
+
+}
