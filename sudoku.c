e3423e860774d2595592bbfc9abc5594b4ec5caa
Spencer <spencerlommel@gmail.com>
2024-09-15 15:43:56 -0500

    get_int_arr_of_section

--- sudoku.c
+++ sudoku.c
@@ -7,19 +7,22 @@
 #include "sudoku.h"
 #include <math.h>
 #include <stdbool.h>
+#include <stdint.h>
 #include <stdlib.h>
 
 // Necessary so people can index this with the error value
 // from their program to know what the issue is.
 const char *sudoku_error_strings[] = {"NO_ERROR", "NULL_PTR_ERROR"};
 
-// Header (public) methods
+typedef enum { ROW = 0, COLUMN, BOX } sudoku_section_t;
+
+// Header methods
 sudoku_error_t sudoku_init(sudoku_board_t *sudoku_board) {
   if (sudoku_board == NULL) {
     return NULL_PTR_ERROR;
   }
-  for (int i = 0; i < 9; i++) {
-    for (int j = 0; j < 9; j++) {
+  for (uint8_t i = 0; i < 9; i++) {
+    for (uint8_t j = 0; j < 9; j++) {
       sudoku_board->board[j][i] = 0;
     }
   }
@@ -27,15 +30,15 @@ sudoku_error_t sudoku_init(sudoku_board_t *sudoku_board) {
 }
 
 sudoku_error_t sudoku_generate_filled(sudoku_board_t *sudoku_board) {
-  for (int y = 0; y < 9; y++) {
-    for (int x = 0; x < 9; x++) {
+  for (uint8_t y = 0; y < 9; y++) {
+    for (uint8_t x = 0; x < 9; x++) {
       sudoku_board->board[x][y];
     }
   }
 }
 
 // Static methods
-static int get_box_value_from_cell(int cell) {
+static uint8_t get_box_value_from_cell(uint8_t cell) {
   // This takes in a cell and returns the first value in the box
   // Example: cell 5 is the second box (3x3) which starts at cell 4
   // so returns 4
@@ -46,7 +49,7 @@ static int get_box_value_from_cell(int cell) {
   return new_value = (ceil(0.333 * cell) * 3) - 2;
 }
 
-int gen_random_with_exclusion_arr(int excluded[9]) {
+int gen_random_with_exclusion_arr(uint8_t excluded[9]) {
   // Takes int[9] input in the format {1,1,0,0,0,0,0,0,0}
   // With the example array above it can return any value between 3-9 but not
   // 1,2 because they are excluded (denoted by 1) I know.. goofy syntax but it
@@ -54,7 +57,7 @@ int gen_random_with_exclusion_arr(int excluded[9]) {
   int excluded_amt = 0;
   int allowed_ints[9];
 
-  for (int i = 0; i < 9; i++) {
+  for (uint8_t i = 0; i < 9; i++) {
     if (excluded[i] == 0) {
       allowed_ints[excluded_amt] = i + 1;
       excluded_amt++;
@@ -74,4 +77,29 @@ static bool valueinarray(float val, float *arr, size_t n) {
       return true;
   }
   return false;
+}
+
+static int *get_int_arr_of_section(sudoku_board_t *sudoku_board,
+                                   sudoku_section_t section, uint8_t x,
+                                   uint8_t y) {
+  uint8_t section_values[9] = {-1};
+  if (section == ROW) {
+    for (uint8_t i = 0; i < 9; i++) {
+      section_values[i] = sudoku_board->board[i][y];
+    }
+  }
+  if (section == COLUMN) {
+    for (uint8_t i = 0; i < 9; i++) {
+      section_values[i] = sudoku_board->board[x][i];
+    }
+  }
+  if (section == BOX) {
+    for (uint8_t i = 0; i < 3; i++) {
+      for (uint8_t k = 0; k < 3; k++) {
+        section_values[i + k] = sudoku_board->board[i][k];
+      }
+    }
+  }
+
+  return section_values;
 }
